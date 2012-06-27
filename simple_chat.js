61534679d278d35dd5a9e37bbe0f36331313342d
sunus lee <sunuslee@gmail.com>
2012-06-27 15:31:31 +0800

    first commit. a simpe websocket chat room.

---
+++ simple_chat.js
@@ -0,0 +1,44 @@
+function init() {
+    admin_name = 'sunussys'
+    function showMsg(string) {
+        var recvMsgarea = document.getElementById("recvMsgarea");
+        recvMsgarea.value += ('\n' + string);
+        //recvMsgarea.scrollTop = recvMsgarea.scrollHeight - recvMsgarea.clientHeight;
+        recvMsgarea.scrollTop = 99999;
+    }
+
+    var Socket = "MozWebSocket" in window ? MozWebSocket : WebSocket;
+    var ws = new Socket("ws://localhost:8080/");
+        ws.onmessage = function(evt) { showMsg(evt.data); };
+        ws.onclose = function() { showMsg("socket closed"); };
+        ws.onopen = function() {
+            showMsg("connected...");
+        };
+
+        window.onload = function() {
+            var btn_send = document.getElementById('btn-send');
+            btn_send.addEventListener("click", function () {
+                var text = document.getElementById('sendMsgarea').value;
+                if(text.length > 0) {
+                    var to = document.getElementById('to').value;
+                    var from = document.getElementById('from').value;
+                    ws.send(from + '|' + to + '|:' + text);
+                    document.getElementById('sendMsgarea').value='';
+                    showMsg('\nYou:\n' + text);
+                }
+            });
+            var btn_start = document.getElementById('btn-start');
+            btn_start.addEventListener("click", function() {
+                var btn_start = document.getElementById('from');
+                var from = btn_start.value;
+                if(from != '')  {
+                    ws.send(from + '|' + admin_name + '||:' + 'login');
+                    alert('welcome\n' + from);
+                    document.getElementById('btn-start').disabled = true;
+                    document.getElementById('from').disabled = true;
+                }
+            });
+        };
+};
+
+init();
---
+++ simple_chat.rb
@@ -0,0 +1,57 @@
+require File.expand_path('../../lib/em-websocket', __FILE__)
+
+$wss = {}
+
+# waiting[user_A] = [user_B1, user_B2, ...]
+# means user_B is waiting for user_A to getting online.
+# so when a user(user_A) gets online, looks up for all the values that has key 'user_B'
+$waiting = {} 
+def send_notification(notification_type, user)
+    if notification_type == 'login'
+        if $waiting.has_key?(user) == true
+            user_bs = $waiting[user]
+            user_bs.each do |u|
+                $wss[u].send("NOTIFY: #{user} is ONLINE now")
+                $wss[user].send("NOTIFY: #{u} is waiting for you now")
+            end
+        end
+    end
+end
+
+def message_handler(ws, msg)
+    puts 'get msg', msg
+    from, to = msg.split('|:')[0].split('|')[0..1]
+    message  = msg.split('|:')[1]
+
+    # create and save a websocket object.
+    if $wss.has_key?(from) == false
+        puts 'get from:', from, ws
+        $wss[from] = ws
+    end
+
+    if to == 'sunussys' and message == 'login'
+        send_notification(message, from)
+    end
+    message  = from + ":\n" + message
+
+    if $wss.has_key?(to) == true
+        puts 'get to', to, ws
+        $wss[to].send(message)
+    # don't send any message related to user 'sunussys'
+    elsif to != 'sunussys'
+        ws.send("NOTIFY: #{to} is currently offline")
+        puts "NOTIFY: #{to} is currently offline"
+        ($waiting[to] ||= []) << from
+    end
+end
+
+EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
+    ws.onopen    { ws.send "Hello Client!" }
+    ws.onmessage do |msg|
+        begin
+            message_handler(ws, msg)
+        rescue
+        end
+    ws.onclose   { puts "WebSocket closed" }
+    ws.onerror   { |e| puts "Error: #{e.message}" }
+end
